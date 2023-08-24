7d92cb390dfa0b35995f23b378032469acfe29db
vava_raul <raul@vavasoftware.com>
2023-08-24 16:16:59 +0800

    tried Route::any and route optional parameters and invokable controller

---
+++ app/Http/Controllers/AnyVerbController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AnyVerbController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request, string $param = null)
+    {
+        return [
+            'key' => 'value',
+            'request' => $request,
+            'param' => $param
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AnyVerbController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/basic-route', function () {
+    return 'Basic route!';
+});
+
+Route::any('/any-verb-here/{param?}', AnyVerbController::class);
