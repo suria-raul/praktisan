c4f0711e68a0ef118aac9f270e36d996075b7f5a
vava_raul <raul@vavasoftware.com>
2023-08-28 13:43:20 +0800

    tested tightenco ziggy

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,5 +3,5 @@ import './bootstrap';
 Echo.channel('login-notification')
     .listen('LoginNotification', (e) => {
         alert('You have been logged out!')
-        window.location.href = '/login'
+        window.location.href = route('viewLogin')
     })
